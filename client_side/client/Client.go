78bb526b050a6a41f8a0750ee734debb7c483a81
Aatrox <knn484@gmail.com>
2022-06-30 13:00:22 +0800

    feat: Implement function of the choice of client
    feature:
    	implement public chat, private chat and rename

--- client_side/client/Client.go
+++ client_side/client/Client.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -31,17 +33,19 @@ func NewClient(serverIP string, serverPort int64) (client *Client) {
 }
 
 func (client *Client) Run() {
+	go client.ReadMsgFromServer()
+
 	for client.flag != 0 {
 		for client.ShowMenu() != true {
 		}
 
 		switch client.flag {
 		case 1:
-			fmt.Printf("Join Chat")
+			client.PublicChat()
 		case 2:
-			fmt.Printf("Private Message")
+			client.PrivateChat()
 		case 3:
-			fmt.Printf("Rename")
+			client.Rename()
 		}
 	}
 }
@@ -64,6 +68,91 @@ func (client *Client) ShowMenu() (ok bool) {
 		ok = false
 		fmt.Println("Illegal input")
 	}
+
+	return
+}
+
+func (client *Client) ReadMsgFromServer() {
+	io.Copy(os.Stdout, client.Conn)
+
+	// same as
+
+	// for {
+	// 	msg := make([]byte, 4096)
+	// 	client.Conn.Read(msg)
+
+	// 	fmt.Print(string(msg))
+	// }
+}
+
+func (client *Client) SendMsgToServer(msg string) (ok bool) {
+	ok = true
+
+	_, err := client.Conn.Write([]byte(fmt.Sprintf("%s\n", msg)))
+	if err != nil {
+		fmt.Printf("client.Conn.Write err: %v", err)
+		ok = false
+	}
+	return
+}
+
+func (client *Client) PublicChat() (ok bool) {
+	var inputMsg string
+	fmt.Println("Insert message, \"exit\" to leave:")
+	fmt.Scanln(&inputMsg)
+
+	for inputMsg != "exit" {
+
+		if len(inputMsg) != 0 {
+			ok = client.SendMsgToServer(inputMsg)
+			if !ok {
+				break
+			}
+		}
+
+		inputMsg = ""
+		fmt.Println("Insert message, \"exit\" to leave:")
+		fmt.Scanln(&inputMsg)
+	}
+	return
+}
+
+func (client *Client) PrivateChat() (ok bool) {
+	var targetName string
+	var msg string
+
+	ok = client.ListUsers()
+	fmt.Println("Insert target username, \"exit\" to leave:")
+	fmt.Scanln(&targetName)
+
+	if targetName != "exit" {
+		fmt.Println("Insert message, \"exit\" to leave:")
+		fmt.Scanln(&msg)
+
+		for msg != "exit" {
+			msg = fmt.Sprintf("dm:%s %s", targetName, msg)
+			client.SendMsgToServer(msg)
+
+			msg = ""
+			fmt.Println("Insert message, \"exit\" to leave:")
+			fmt.Scanln(&msg)
+		}
+	}
+
+	return
+}
+
+func (client *Client) ListUsers() (ok bool) {
+	ok = client.SendMsgToServer("who")
+	return
+}
+
+func (client *Client) Rename() (ok bool) {
+	fmt.Print("Input new name: ")
+	fmt.Scanln(&client.Name)
+
+	msg := fmt.Sprintf("rename %s", client.Name)
+	ok = client.SendMsgToServer(msg)
 
 	return
 }
